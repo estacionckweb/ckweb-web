a59c8896388558b14be15b243a47c8b9ecb4b9fb
OLIJAC <ojackson@gmail.com>
2018-10-04 13:45:29 -0500

    build folder

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var fs = require('fs');
+
+gulp.task('public', function(){
+  return gulp.src('public/*')
+    .pipe(gulp.dest('build'))
+});
+
+gulp.task('js', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './app/index.js',
+    debug: true,
+  });
+
+  return b.bundle()
+    .pipe(fs.createWriteStream('build/bundle.js'));
+});
+
+gulp.task('build', [ 'public', 'js' ]);
