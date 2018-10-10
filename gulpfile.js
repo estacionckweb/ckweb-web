2c6a5a3898558f6a5dc5eeb1ed6eba6716d5f03e
Olivia Jack <ojackson@gmail.com>
2018-10-10 15:30:01 -0500

    got rid of build folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var fs = require('fs');
 
-gulp.task('public', function(){
-  return gulp.src('public/*')
-    .pipe(gulp.dest('build'))
-});
+// gulp.task('public', function(){
+//   return gulp.src('public/*')
+//     .pipe(gulp.dest('build'))
+// });
 
 gulp.task('js', function () {
   // set up the browserify instance on a task basis
@@ -15,7 +15,7 @@ gulp.task('js', function () {
   });
 
   return b.bundle()
-    .pipe(fs.createWriteStream('build/bundle.js'));
+    .pipe(fs.createWriteStream('public/bundle.js'));
 });
 
-gulp.task('build', [ 'public', 'js' ]);
+gulp.task('build', [ 'js' ]);
