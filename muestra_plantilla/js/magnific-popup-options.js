fa771f2cbab25f228ba40a38a9c86d5a6689d6cc
anattolia <anattolia@gmail.com>
2020-11-10 19:18:20 -0500

    aaa3

--- muestra_plantilla/js/magnific-popup-options.js
+++ muestra_plantilla/js/magnific-popup-options.js
@@ -31,3 +31,21 @@ $(document).ready(function() {
   magnifPopup();
 
 });
+
+function displayLink() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function displayLink2() {
+  var x = document.getElementById("myDIV2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
